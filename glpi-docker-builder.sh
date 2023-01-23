5af5fc3a932292ebce890a4c2d8074ad4c94ba09
Szczepan Rędzioch <>
2023-01-23 22:25:12 +0100

    WIP

--- glpi-docker-builder.sh
+++ glpi-docker-builder.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "Test"
+# read config
+GLPI_DOCKER_REPO=$(yq '.glpi-docker.repository.url' glpi-version.yml)
+GLPI_REPO=$(yq '.glpi.repository.url' glpi-version.yml)
+GLPI_TAG=$(yq '.glpi.tag' glpi-version.yml)
 
-echo ~
+echo $GLPI_DOCKER_REPO
+echo $GLPI_REPO
+echo $GLPI_TAG
